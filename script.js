e1fe0917b902ff5a13bae7c652121043f49fde05
Finn Nguon <36335479+Feenerys@users.noreply.github.com>
2025-01-29 17:16:54 +1100

    feat: randomly return rock, paper, scissors as strings

---
+++ script.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    // randomly return rock, paper, or scissors as strings
+    let choice = Math.floor(Math.random() * 3);
+    switch (choice) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+}
